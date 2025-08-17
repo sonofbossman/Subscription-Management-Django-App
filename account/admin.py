d9aeb1c96b31d3cab5b8f52f713407915336ac69
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-17 15:25:20 +0100

    complete admin panel setup

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from account.models import Profile, CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+  list_display = ('id', 'username', 'first_name', 'last_name', 
+              'email', 'is_staff', 'is_active', 'last_login', 'date_joined'
+              )
+  list_filter = ('is_staff', 'is_active')
+  search_fields = ('id', 'email', 'first_name', 'last_name', 'username')
+
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ('id', 'user', 'full_name', 'date_of_birth', 'date_created', 'date_updated')
+  search_fields = ('id', 'date_of_birth', 'full_name', 'user')
+  list_filter = ('date_of_birth','date_created')
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- account/models.py
+++ account/models.py
@@ -13,6 +13,8 @@ class Profile(models.Model):
   full_name = models.CharField(max_length=255, blank=True)
   date_of_birth = models.DateField(null=True, blank=True)
   profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
+  date_created = models.DateTimeField(auto_now_add=True)
+  date_updated = models.DateTimeField(auto_now=True, null=True)
 
   def __str__(self):
     return f"{self.user.username}'s profile"
