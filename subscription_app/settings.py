7199577b2b9fdeaf7ead1b6d1e273a505ed1fc93
sonofbossman <akingbadeeniola452@gmail.com>
2025-08-31 21:20:42 +0100

    prepare the app for deployment on render

--- subscription_app/settings.py
+++ subscription_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_uzk^$17y*q@%l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), '127.0.0.0']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
 
 
 # Application definition
